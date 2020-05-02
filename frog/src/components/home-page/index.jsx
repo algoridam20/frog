5a8e822914db8178461ce0e7d0074bce10168224
Ridam Jain <ridam.jain@walmartlabs.com>
2020-05-03 03:20:00 +0530

    re-render header on path change

--- frog/src/components/home-page/index.jsx
+++ frog/src/components/home-page/index.jsx
@@ -3,14 +3,18 @@ import dictionary from "../../dictionary.json";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { Wrapper } from "./styles";
+import { Header } from "../common/header";
 
 export const HomePage = () => {
   return (
-    <Wrapper>
-      {" "}
-      <Typography variant="h5">{dictionary.workInProgress}</Typography>{" "}
-      <Link to={"/graph-visualizer"}>{"until then navigate here"}</Link>{" "}
-      <Link to={"/knowledge-graph/graph1"}>{"or here"}</Link>
-    </Wrapper>
+    <React.Fragment>
+      <Header />
+      <Wrapper>
+        {" "}
+        <Typography variant="h5">{dictionary.workInProgress}</Typography>{" "}
+        <Link to={"/graph-visualizer"}>{"until then navigate here"}</Link>{" "}
+        <Link to={"/knowledge-graph/graph1"}>{"or here"}</Link>
+      </Wrapper>
+    </React.Fragment>
   );
 };
--- frog/src/components/knowledge-graph-page/index.jsx
+++ frog/src/components/knowledge-graph-page/index.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import dictionary from "../../dictionary.json";
 import { Typography } from "@material-ui/core";
 import { Wrapper } from "./styles";
+import { Header } from "../common/header";
 
 export const KnowledgeGraph = ({ match }) => {
   const {
     params: { graphId },
   } = match;
   return (
-    <Wrapper>
-      <Typography variant="h5">
-        Graph # {graphId} {dictionary.workInProgress}
-      </Typography>
-    </Wrapper>
+    <React.Fragment>
+      <Header />
+      <Wrapper>
+        <Typography variant="h5">
+          Graph # {graphId} {dictionary.workInProgress}
+        </Typography>
+      </Wrapper>
+    </React.Fragment>
   );
 };
