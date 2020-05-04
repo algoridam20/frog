30e53c77d54fa3df28f78a9fbc1a1d77b58250e2
algoridam20 <ridammj@gmail.com>
2020-05-04 07:08:57 +0530

    add edges (temp way)

--- frog/src/components/common/graph-grid/index.jsx
+++ frog/src/components/common/graph-grid/index.jsx
@@ -8,39 +8,86 @@ import {
   Wrapper,
 } from "./styles";
 import { Node } from "../node";
+import { EdgeByIds } from "../edge";
 
-export const GraphGrid = ({
-  row,
-  col,
-  editMode = false,
-  elementSize = "90px",
-}) => {
-  return (
-    <Wrapper>
-      <GridWrapper>
-        {/* <CardContainer> */}
-        <GridContainerWrapper row={row} col={col} elementSize={elementSize}>
-          <GridChildWrapper colIndex={4 % col} rowIndex={3 % row}>
-            <Node name={"A"} size={0} color={"red"} />
-          </GridChildWrapper>
-          <GridChildWrapper colIndex={2 % col} rowIndex={6 % row}>
-            <Node name={"B"} size={1} color={"red"} />
-          </GridChildWrapper>
-          <GridChildWrapper colIndex={9 % col} rowIndex={6 % row}>
-            <Node name={"109"} size={2} color={"red"} />
-          </GridChildWrapper>
-          <GridChildWrapper colIndex={4 % col} rowIndex={2 % row}>
-            <Node name={"A"} size={3} color={"red"} />
-          </GridChildWrapper>
-          <GridChildWrapper colIndex={2 % col} rowIndex={4 % row}>
-            <Node name={"B"} size={4} color={"red"} />
-          </GridChildWrapper>
-          <GridChildWrapper colIndex={1 % col} rowIndex={2 % row}>
-            <Node name={"109"} size={5} color={"red"} />
-          </GridChildWrapper>
-        </GridContainerWrapper>
-        {/* </CardContainer> */}
-      </GridWrapper>
-    </Wrapper>
-  );
-};
+export class GraphGrid extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    EdgeByIds({ fromId: "Node-A", toId: "Node-B", color: "red" });
+    EdgeByIds({ fromId: "Node-A", toId: "Node-E", color: "blue" });
+    EdgeByIds({ fromId: "Node-E", toId: "Node-B", color: "blue" });
+    EdgeByIds({ fromId: "Node-C", toId: "Node-B", color: "cyan" });
+    EdgeByIds({ fromId: "Node-Z", toId: "Node-B", color: "red" });
+    EdgeByIds({ fromId: "Node-A", toId: "Node-Z", color: "green" });
+    EdgeByIds({ fromId: "Node-Z", toId: "Node-D", color: "blue" });
+    EdgeByIds({ fromId: "Node-D", toId: "Node-C", color: "orange" });
+  }
+  render() {
+    const { row, col, editMode = false, elementSize = "90px" } = this.props;
+    return (
+      <Wrapper>
+        <GridWrapper>
+          <GridContainerWrapper row={row} col={col} elementSize={elementSize}>
+            <GridChildWrapper colIndex={4 % col} rowIndex={3 % row}>
+              <Node name={"A"} size={0} color={"red"} />
+            </GridChildWrapper>
+            <GridChildWrapper colIndex={2 % col} rowIndex={6 % row}>
+              <Node name={"B"} size={1} color={"blue"} />
+            </GridChildWrapper>
+            <GridChildWrapper colIndex={9 % col} rowIndex={6 % row}>
+              <Node name={"E"} size={2} color={"orange"} />
+            </GridChildWrapper>
+            <GridChildWrapper colIndex={4 % col} rowIndex={2 % row}>
+              <Node name={"C"} size={3} color={"red"} />
+            </GridChildWrapper>
+            <GridChildWrapper colIndex={2 % col} rowIndex={4 % row}>
+              <Node name={"D"} size={4} color={"cyan"} />
+            </GridChildWrapper>
+            <GridChildWrapper colIndex={1 % col} rowIndex={2 % row}>
+              <Node name={"Z"} size={5} color={"green"} />
+            </GridChildWrapper>
+            {/* <EdgeByIds fromId={"Node-A"} toId={"Node-B"} /> */}
+          </GridContainerWrapper>
+        </GridWrapper>
+      </Wrapper>
+    );
+  }
+}
+// export const GraphGrid = ({
+//   row,
+//   col,
+//   editMode = false,
+//   elementSize = "90px",
+// }) => {
+//   return (
+//     <Wrapper>
+//       <GridWrapper>
+//         {/* <CardContainer> */}
+//         <GridContainerWrapper row={row} col={col} elementSize={elementSize}>
+//           <GridChildWrapper colIndex={4 % col} rowIndex={3 % row}>
+//             <Node name={"A"} size={0} color={"red"} />
+//           </GridChildWrapper>
+//           <GridChildWrapper colIndex={2 % col} rowIndex={6 % row}>
+//             <Node name={"B"} size={1} color={"red"} />
+//           </GridChildWrapper>
+//           <GridChildWrapper colIndex={9 % col} rowIndex={6 % row}>
+//             <Node name={"109"} size={2} color={"red"} />
+//           </GridChildWrapper>
+//           <GridChildWrapper colIndex={4 % col} rowIndex={2 % row}>
+//             <Node name={"A"} size={3} color={"red"} />
+//           </GridChildWrapper>
+//           <GridChildWrapper colIndex={2 % col} rowIndex={4 % row}>
+//             <Node name={"B"} size={4} color={"red"} />
+//           </GridChildWrapper>
+//           <GridChildWrapper colIndex={1 % col} rowIndex={2 % row}>
+//             <Node name={"109"} size={5} color={"red"} />
+//           </GridChildWrapper>
+//           <EdgeByIds fromId={"Node-A"} toId={"Node-B"} />
+//         </GridContainerWrapper>
+//         {/* </CardContainer> */}
+//       </GridWrapper>
+//     </Wrapper>
+//   );
+// };
