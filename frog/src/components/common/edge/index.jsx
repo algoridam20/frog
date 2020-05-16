cd158ecc1139c304e150ce64600c5c331b0f0219
algoridam20 <ridammj@gmail.com>
2020-05-17 04:44:35 +0530

    add edges without canvas

--- frog/src/components/common/edge/index.jsx
+++ frog/src/components/common/edge/index.jsx
@@ -1,67 +1,31 @@
 import React from "react";
 import { getPositionById } from "../../../utils/find-position-by-id";
-import { EdgeWrapper } from "./styles";
+import { Wrapper } from "./styles";
 
-export const EdgeByIds = ({
-  fromId,
-  toId,
+export const EdgeByGridCoordinates = ({
+  gridElementSize,
+  fromRow,
+  fromCol,
+  toRow,
+  toCol,
   color,
   isDirected = false,
   weight = 0,
 }) => {
-  //   return "";
-  const fromNode = getPositionById(fromId);
-  const toNode = getPositionById(toId);
-  console.log(fromNode, toNode, ".........");
-  drawCurvedLine(fromNode.x, fromNode.y, toNode.x, toNode.y, color, 0.0);
-  return "";
-};
-const drawCurvedLine = (x1, y1, x2, y2, color, tension) => {
-  let svg = createSVG();
-  let shape = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  let delta = (x2 - x1) * tension;
-  let hx1 = x1 + delta;
-  let hy1 = y1;
-  let hx2 = x2 - delta;
-  let hy2 = y2;
-  let path =
-    "M " +
-    x1 +
-    " " +
-    y1 +
-    " C " +
-    hx1 +
-    " " +
-    hy1 +
-    " " +
-    hx2 +
-    " " +
-    hy2 +
-    " " +
-    x2 +
-    " " +
-    y2;
-  shape.setAttributeNS(null, "d", path);
-  shape.setAttributeNS(null, "fill", "none");
-  shape.setAttributeNS(null, "stroke", color);
-  svg.appendChild(shape);
-};
+  const delX = fromCol - toCol;
+  const delY = fromRow - toRow;
+  // ye logic sahi kar de 👀
+  const angleCorrection =
+    (fromRow < toRow && fromCol > toCol) ||
+    (fromRow === toRow && fromCol > toCol)
+      ? 180
+      : 0;
+  console.log(angleCorrection);
+  const degrees = Math.atan(delY / delX) * (180 / Math.PI) + angleCorrection;
+  const length =
+    Math.sqrt(delX * delX + delY * delY) * gridElementSize.replace("px", "");
 
-const createSVG = () => {
-  let svg = document.getElementById("svg-canvas");
-  if (null == svg) {
-    svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("id", "svg-canvas");
-    svg.setAttribute("style", "position:absolute;top:0px;left:0px");
-    svg.setAttribute("width", document.body.clientWidth);
-    svg.setAttribute("height", document.body.clientHeight);
-    svg.setAttribute("z-index", "-1");
-    svg.setAttributeNS(
-      "http://www.w3.org/2000/xmlns/",
-      "xmlns:xlink",
-      "http://www.w3.org/1999/xlink"
-    );
-    document.body.appendChild(svg);
-  }
-  return svg;
+  return (
+    <Wrapper row={fromRow} col={fromCol} degrees={degrees} length={length} />
+  );
 };
