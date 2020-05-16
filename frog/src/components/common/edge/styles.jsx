704f94e42b6acbd5a466f4f94549235dd29956c0
Ridam Jain <ridam.jain@walmartlabs.com>
2020-05-17 04:44:35 +0530

    add edges without canvas

--- frog/src/components/common/edge/styles.jsx
+++ frog/src/components/common/edge/styles.jsx
@@ -2,4 +2,16 @@
 
 import styled from "styled-components";
 
-export const EdgeWrapper = styled.div``;
+
+export const Wrapper = styled.div`
+  grid-column: ${(props) => props.col};
+  grid-row: ${(props) => props.row};
+  :after {
+    content: "";
+    position: absolute;
+    border-top: 1px solid red;
+    width: ${(props) => props.length}px;
+    transform: rotate(${(props) => props.degrees}deg);
+    transform-origin: 0% 0%;
+  }
+`;
