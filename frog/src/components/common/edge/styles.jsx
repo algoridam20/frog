d3fa540ab9b87f13491e8b2226fca8ae5f4cfe76
Ridam Jain <ridam.jain@walmartlabs.com>
2020-05-23 07:16:52 +0530

    add sample graphs

--- frog/src/components/common/edge/styles.jsx
+++ frog/src/components/common/edge/styles.jsx
@@ -2,16 +2,30 @@
 
 import styled from "styled-components";
 
-
 export const Wrapper = styled.div`
   grid-column: ${(props) => props.col};
   grid-row: ${(props) => props.row};
-  :after {
-    content: "";
-    position: absolute;
-    border-top: 1px solid red;
-    width: ${(props) => props.length}px;
-    transform: rotate(${(props) => props.degrees}deg);
-    transform-origin: 0% 0%;
-  }
+`;
+
+export const EdgeWrapper = styled.div`
+  z-index: -1;
+  align-content: center;
+  content: "";
+  position: absolute;
+  height: 3px;
+  margin-bottom: 5px;
+  border: 3px solid ${(props) => props.color};
+  width: ${(props) => props.length}px;
+  transform: rotate(${(props) => props.degrees}deg);
+  transform-origin: left;
+`;
+
+export const TextWrapper = styled.div`
+  vertical-align: middle;
+  font-weight: 600;
+  z-index: 100;
+  margin-right: 27%;
+  line-height: 5px;
+  text-align: right;
+  font-size: 15px;
 `;
