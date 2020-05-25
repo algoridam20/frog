28b43fe5b3513d59bc28ca9e4fc9b34e0b03717a
algoridam20 <ridammj@gmail.com>
2020-05-25 21:03:56 +0530

    add redux-saga boiler-plate code

--- frog/src/components/graph-visualizer-page/index.jsx
+++ frog/src/components/graph-visualizer-page/index.jsx
@@ -6,35 +6,20 @@ import { CardContainer } from "../common/card";
 import { Header } from "../common/header";
 import { GraphGrid } from "../common/graph-grid";
 import { graph1 } from "../../state/sample-graphs";
-
-// export const GraphVisualizer = () => {
-//   return (
-//     <React.Fragment>
-//       <Header />
-//       <Wrapper>
-//         <ToolBarWrapper>
-//           <CardContainer title={"Toolbar"}>
-//             {dictionary.workInProgress}
-//           </CardContainer>
-//         </ToolBarWrapper>
-//         <ContentWrapper>
-//           <CardContainer title={"Graph"}>
-//             <GraphGrid totalRow={8} totalCol={13} graph={graph1} />
-//           </CardContainer>
-//         </ContentWrapper>
-//       </Wrapper>
-//     </React.Fragment>
-//   );
-// };
+import { runGraphAlgoRequest } from "../../state/actions/graph-visualizer-actions";
 
 class GraphVisualizer extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidUpdate() {}
+  componentDidMount() {
+    this.props.runGraphAlgoRequest("dfs");
+  }
 
   render() {
+    const { isRunning, algo, error } = this.props.algorithm;
+    console.log(isRunning, algo, error, ".....");
     return (
       <React.Fragment>
         <Header />
@@ -56,13 +41,13 @@ class GraphVisualizer extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  graph: state.graph,
+  algorithm: state.algorithm,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  // fetchOrderDetails(orderId) {
-  //   dispatch();
-  // }
+  runGraphAlgoRequest(algo) {
+    dispatch(runGraphAlgoRequest(algo));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(GraphVisualizer);
